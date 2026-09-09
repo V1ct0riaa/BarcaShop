a24ce17816870287e557156d479e387229d1cbff
Natanael Pascal <npascal70@gmail.com>
2026-09-09 23:18:57 +0700

    feat:adding show_xml

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from main.views import create_product, show_main, show_product
+from main.views import create_product, show_main, show_product, show_xml
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("create-product/", create_product, name="create_product"),
-    path("product/<str:id>", show_product, name="show_product")
+    path("product/<str:id>", show_product, name="show_product"),
+    path("xml/,", show_xml, name="show_xml"),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from main.forms import ProductsForm
@@ -6,8 +8,12 @@ from main.models import Product
 
 # Create your views here.
 def show_main(request):
-    product_list = Product.objects.all() # take all Product object from db
-    context = {"app_name": "BarcaShop", "name": "Natanael Pascal", "product_list": product_list}
+    product_list = Product.objects.all()  # take all Product object from db
+    context = {
+        "app_name": "BarcaShop",
+        "name": "Natanael Pascal",
+        "product_list": product_list,
+    }
 
     return render(request, "main.html", context)
 
@@ -17,17 +23,23 @@ def create_product(request):
 
     if form.is_valid() and request.method == "POST":
         form.save()
-        return redirect("main:show_main") # searches for a path name in /main where name=show_main
+        return redirect(
+            "main:show_main"
+        )  # searches for a path name in /main where name=show_main
 
     context = {"form": form}
     return render(request, "create_product.html", context)
+
 
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
     product.increment_views()
 
-    context = {
-        'product': product
-    }
+    context = {"product": product}
 
     return render(request, "product_detail.html", context)
+
+
+def show_xml(request):
+    product_list = Product.objects.all()
+    xml_data = serializers.serialize("xml", product_list)
